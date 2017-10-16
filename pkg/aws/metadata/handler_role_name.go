36b9f90cd96e34a10d77d9200ed7d28d6dd9f4f8
Paul Ingles <paul@oobaloo.co.uk>
2017-10-16 11:35:01 +0100

    remove backoff rety behaviour in credential handler for now... going to tidy it all up

--- pkg/aws/metadata/handler_role_name.go
+++ pkg/aws/metadata/handler_role_name.go
@@ -18,7 +18,7 @@ import (
 	"github.com/rcrowley/go-metrics"
 	log "github.com/sirupsen/logrus"
 	khttp "github.com/uswitch/kiam/pkg/http"
-	"github.com/vmg/backoff"
+	// "github.com/vmg/backoff"
 	"net/http"
 	"time"
 )
@@ -43,58 +43,19 @@ func (s *Server) roleNameHandler(w http.ResponseWriter, req *http.Request) (int,
 		return http.StatusInternalServerError, fmt.Errorf("error parsing ip: %s", err.Error())
 	}
 
-	respCh := make(chan *asyncObj)
-	go func() {
-		roleCh := make(chan string, 1)
-		op := func() error {
-			role, err := s.finder.FindRoleFromIP(req.Context(), ip)
-			if err != nil {
-				return err
-			}
-
-			if role == "" {
-				requestLog.Warnf("no pod found for ip")
-				return PodNotFound
-			}
-
-			roleCh <- role
-			return nil
-		}
-
-		strategy := backoff.NewExponentialBackOff()
-		strategy.MaxElapsedTime = s.cfg.MaxElapsedTime
-
-		err = backoff.Retry(op, backoff.WithContext(strategy, req.Context()))
-		if err != nil {
-			respCh <- &asyncObj{obj: nil, err: err}
-		} else {
-			role := <-roleCh
-			respCh <- &asyncObj{obj: role, err: nil}
-		}
-	}()
-
-	select {
-	case <-req.Context().Done():
-		if req.Context().Err() != nil {
-			return http.StatusInternalServerError, req.Context().Err()
-		}
-	case resp := <-respCh:
-		if resp.err == PodNotFound {
-			metrics.GetOrRegisterMeter("roleNameHandler.podNotFound", metrics.DefaultRegistry).Mark(1)
-			return http.StatusNotFound, fmt.Errorf("pod not found for ip %s", ip)
-		} else if resp.err != nil {
-			return http.StatusInternalServerError, fmt.Errorf("error finding pod: %s", err.Error())
-		}
-
-		role := resp.obj.(string)
-		if role == "" {
-			return http.StatusNotFound, fmt.Errorf("no role for pod %s", ip)
-		}
-
-		fmt.Fprint(w, role)
-		metrics.GetOrRegisterMeter("roleNameHandler.success", metrics.DefaultRegistry).Mark(1)
-		return http.StatusOK, nil
+	role, err := s.finder.FindRoleFromIP(req.Context(), ip)
+	if err != nil {
+		return http.StatusInternalServerError, err
 	}
 
+	if role == "" {
+		requestLog.Warnf("no pod found for ip")
+		metrics.GetOrRegisterMeter("roleNameHandler.podNotFound", metrics.DefaultRegistry).Mark(1)
+
+		return http.StatusNotFound, PodNotFound
+	}
+
+	fmt.Fprint(w, role)
+	metrics.GetOrRegisterMeter("roleNameHandler.success", metrics.DefaultRegistry).Mark(1)
 	return http.StatusOK, nil
 }
