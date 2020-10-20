0020fc34fab4c5c899b926562127b3285ab81046
Paul Ingles <paul@oobaloo.co.uk>
2020-10-20 22:49:56 +0100

    healthCommand Lint
    Rename opts to cmd to be consistent

--- cmd/kiam/health.go
+++ cmd/kiam/health.go
@@ -37,19 +37,19 @@ func (cmd *healthCommand) Bind(parser parser) {
 	parser.Flag("timeout", "Timeout for health check").Default("1s").DurationVar(&cmd.timeout)
 }
 
-func (opts *healthCommand) Run() {
-	opts.configureLogger()
+func (cmd *healthCommand) Run() {
+	cmd.configureLogger()
 
-	ctxGateway, cancelCtxGateway := context.WithTimeout(context.Background(), opts.timeoutKiamGateway)
+	ctxGateway, cancelCtxGateway := context.WithTimeout(context.Background(), cmd.timeoutKiamGateway)
 	defer cancelCtxGateway()
 
-	gateway, err := kiamserver.NewGateway(ctxGateway, opts.serverAddress, opts.caPath, opts.certificatePath, opts.keyPath, opts.keepaliveParams)
+	gateway, err := kiamserver.NewGateway(ctxGateway, cmd.serverAddress, cmd.caPath, cmd.certificatePath, cmd.keyPath, cmd.keepaliveParams)
 	if err != nil {
 		log.Fatalf("error creating server gateway: %s", err.Error())
 	}
 	defer gateway.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), opts.timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), cmd.timeout)
 	defer cancel()
 
 	op := func() error {
